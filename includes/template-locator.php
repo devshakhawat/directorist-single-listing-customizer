609eb471a9ef549f6bb82d1db5bd3179240bacc2
Shakhawat Hossain <shakhawat579@gmail.com>
2026-04-14 11:38:49 +0600

    override directorist template

--- includes/template-locator.php
+++ includes/template-locator.php
@@ -12,14 +12,24 @@ defined( 'ABSPATH' ) || exit;
 class Template_Locator {
 
     public function __construct() {
-        add_filter( 'directorist_template_file_path', [ $this, 'override_directorist_template' ], 999 );
+        add_filter( 'directorist_template_file_path', [ $this, 'override_directorist_template' ], 999, 3 );
     }
 
-    public function override_directorist_template( $template ) {
+    public function override_directorist_template( $template, $template_name, $args ) {
 
-        pretty_log( $template, 'ggg' );
+        $overrides = [
+            'single-contents' => 'directory-single-listing-page.php',
+        ];
+
+        if ( isset( $overrides[ $template_name ] ) ) {
+            $plugin_template = SKTPR_PLUGIN_DIR . 'templates/' . $overrides[ $template_name ];
+
+            if ( file_exists( $plugin_template ) ) {
+                return $plugin_template;
+            }
+        }
 
         return $template;
-    } 
+    }
 
 }
